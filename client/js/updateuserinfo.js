54458298f7a9f148bf6323d3c404c4e78d27028e
cozzyde <54963496+cozheyuanzhangde@users.noreply.github.com>
2020-11-25 19:12:12 +0800

    Update updateuserinfo.js

--- client/js/updateuserinfo.js
+++ client/js/updateuserinfo.js
@@ -6,6 +6,9 @@ const letter = document.getElementById("letter");
 const number = document.getElementById("number");
 const length = document.getElementById("length");
 
+document.getElementById("pwdinput").style.visibility = "hidden";
+document.getElementById("pwdconf").style.visibility = "hidden";
+
 // When the user clicks on the password field, show the message box
 userPwdInput.onfocus = function() {
   document.getElementById("message").style.display = "block";
@@ -96,97 +99,100 @@ window.addEventListener("load", async function (){
         usermajor = document.getElementById('usermajor').value;
     });
 
-    userPwdInput.onkeyup = function() {
-      let check1,check2,check3 = false;
-      // Validate letters
-      const letters = /[a-zA-Z]/g;
-      if(userPwdInput.value.match(letters)) {  
-        letter.classList.remove("invalid");
-        letter.classList.add("valid");
-        check1 = true;
-      } else {
-        letter.classList.remove("valid");
-        letter.classList.add("invalid");
-        check1 = false;
-      }
-    
-      // Validate numbers
-      const numbers = /[0-9]/g;
-      if(userPwdInput.value.match(numbers)) {  
-        number.classList.remove("invalid");
-        number.classList.add("valid");
-        check2 = true;
-      } else {
-        number.classList.remove("valid");
-        number.classList.add("invalid");
-        check2 = false;
-      }
+    document.getElementById('pwdinputshow').addEventListener('click',()=>{
+      document.getElementById("pwdinput").style.visibility = "visible";
+      document.getElementById("pwdconf").style.visibility = "visible";
+      userPwdInput.onkeyup = function() {
+        let check1,check2,check3 = false;
+        // Validate letters
+        const letters = /[a-zA-Z]/g;
+        if(userPwdInput.value.match(letters)) {  
+          letter.classList.remove("invalid");
+          letter.classList.add("valid");
+          check1 = true;
+        } else {
+          letter.classList.remove("valid");
+          letter.classList.add("invalid");
+          check1 = false;
+        }
       
-      // Validate length
-      if(userPwdInput.value.length >= 6) {
-        length.classList.remove("invalid");
-        length.classList.add("valid");
-        check3 = true;
-      } else {
-        length.classList.remove("valid");
-        length.classList.add("invalid");
-        check3 = false;
-      }
-      if((check1 === true)&&((check2 === true))&&((check3 === true))){
-        userPwdInput.classList.remove("btn-outline-danger");
-        userPwdInput.classList.add("btn-outline-success");
-        document.getElementById("pwdalert").style.visibility = "hidden";
-        valid1 = true;
-      }else{
-        userPwdInput.classList.add("btn-outline-danger");
-        userPwdInput.classList.remove("btn-outline-success");
-        document.getElementById("pwdalert").style.visibility = "visible";
-        valid1 = false;
-      }
-
-      if(userPwdConfInput.value === userPwdInput.value){
-        userPwdConfInput.classList.remove("btn-outline-danger");
-        userPwdConfInput.classList.add("btn-outline-success");
-        document.getElementById("confpwdalert").style.visibility = "hidden";
-        valid2 = true;
-      }else{
-        userPwdConfInput.classList.add("btn-outline-danger");
-        userPwdConfInput.classList.remove("btn-outline-success");
-        document.getElementById("confpwdalert").style.visibility = "visible";
-        valid2 = false;
-      }
-
-      if((userPwdInput.value.length === 0)&&(userPwdConfInput.value.length === 0)){
-        document.getElementById("submit").disabled = false;
-      }else if((valid1 === true)&&(valid2 === true)){
-        document.getElementById("submit").disabled = false;
-      }else{
-        document.getElementById("submit").disabled = true;
-      }
-    };
-
-    userPwdConfInput.onkeyup = function() {
-      if(userPwdConfInput.value === userPwdInput.value){
-        userPwdConfInput.classList.remove("btn-outline-danger");
-        userPwdConfInput.classList.add("btn-outline-success");
-        document.getElementById("confpwdalert").style.visibility = "hidden";
-        valid2 = true;
-      }else{
-        userPwdConfInput.classList.add("btn-outline-danger");
-        userPwdConfInput.classList.remove("btn-outline-success");
-        document.getElementById("confpwdalert").style.visibility = "visible";
-        valid2 = false;
-      }
-
-      if((userPwdInput.value.length === 0)&&(userPwdConfInput.value.length === 0)){
-        document.getElementById("submit").disabled = false;
-      }else if((valid1 === true)&&(valid2 === true)){
-        document.getElementById("submit").disabled = false;
-      }else{
-        document.getElementById("submit").disabled = true;
-      }
-    };
-
+        // Validate numbers
+        const numbers = /[0-9]/g;
+        if(userPwdInput.value.match(numbers)) {  
+          number.classList.remove("invalid");
+          number.classList.add("valid");
+          check2 = true;
+        } else {
+          number.classList.remove("valid");
+          number.classList.add("invalid");
+          check2 = false;
+        }
+        
+        // Validate length
+        if(userPwdInput.value.length >= 6) {
+          length.classList.remove("invalid");
+          length.classList.add("valid");
+          check3 = true;
+        } else {
+          length.classList.remove("valid");
+          length.classList.add("invalid");
+          check3 = false;
+        }
+        if((check1 === true)&&((check2 === true))&&((check3 === true))){
+          userPwdInput.classList.remove("btn-outline-danger");
+          userPwdInput.classList.add("btn-outline-success");
+          document.getElementById("pwdalert").style.visibility = "hidden";
+          valid1 = true;
+        }else{
+          userPwdInput.classList.add("btn-outline-danger");
+          userPwdInput.classList.remove("btn-outline-success");
+          document.getElementById("pwdalert").style.visibility = "visible";
+          valid1 = false;
+        }
+  
+        if(userPwdConfInput.value === userPwdInput.value){
+          userPwdConfInput.classList.remove("btn-outline-danger");
+          userPwdConfInput.classList.add("btn-outline-success");
+          document.getElementById("confpwdalert").style.visibility = "hidden";
+          valid2 = true;
+        }else{
+          userPwdConfInput.classList.add("btn-outline-danger");
+          userPwdConfInput.classList.remove("btn-outline-success");
+          document.getElementById("confpwdalert").style.visibility = "visible";
+          valid2 = false;
+        }
+  
+        if((userPwdInput.value.length === 0)&&(userPwdConfInput.value.length === 0)){
+          document.getElementById("submit").disabled = false;
+        }else if((valid1 === true)&&(valid2 === true)){
+          document.getElementById("submit").disabled = false;
+        }else{
+          document.getElementById("submit").disabled = true;
+        }
+      };
+  
+      userPwdConfInput.onkeyup = function() {
+        if(userPwdConfInput.value === userPwdInput.value){
+          userPwdConfInput.classList.remove("btn-outline-danger");
+          userPwdConfInput.classList.add("btn-outline-success");
+          document.getElementById("confpwdalert").style.visibility = "hidden";
+          valid2 = true;
+        }else{
+          userPwdConfInput.classList.add("btn-outline-danger");
+          userPwdConfInput.classList.remove("btn-outline-success");
+          document.getElementById("confpwdalert").style.visibility = "visible";
+          valid2 = false;
+        }
+  
+        if((userPwdInput.value.length === 0)&&(userPwdConfInput.value.length === 0)){
+          document.getElementById("submit").disabled = false;
+        }else if((valid1 === true)&&(valid2 === true)){
+          document.getElementById("submit").disabled = false;
+        }else{
+          document.getElementById("submit").disabled = true;
+        }
+      };
+    });
     document.getElementById('submit').addEventListener('click',()=>{
       if (userpassword === ""){
         updateUserInfoNoPWDChange('/updateuserinfonopwdchange', userid, useremail, username, userschoolname, usergender, usermajor);
@@ -194,12 +200,12 @@ window.addEventListener("load", async function (){
         location.reload();
       }else{
         updateUserInfo('/updateuserinfo', userid, useremail, userpassword, username, userschoolname, usergender, usermajor);
-        alert("You have successfully updated your user profile!");
-        location.reload();
+        alert("You have successfully updated your user profile with Password Change! You will be logged out!");
+        window.location.href = '/logout';
       }
     });
   }catch(error){
     alert("Please Login first and then access User Profile!");
     window.location.href="./login.html";
   }
-});
+  });
